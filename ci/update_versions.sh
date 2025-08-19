7d533f3c22e69a71d33e3d49d89ca0c83184a243
Victor Morales <chipahuac@hotmail.com>
2025-08-18 20:21:44 -0700

    Fix identation in bash format

--- ci/update_versions.sh
+++ ci/update_versions.sh
@@ -11,13 +11,13 @@
 set -o errexit
 set -o pipefail
 if [[ ${DEBUG:-false} == "true" ]]; then
-	set -o xtrace
+    set -o xtrace
 fi
 
 if ! command -v go >/dev/null; then
-	curl -fsSL http://bit.ly/install_pkg | PKG=go-lang bash
-	# shellcheck disable=SC1091
-	source /etc/profile.d/path.sh
+    curl -fsSL http://bit.ly/install_pkg | PKG=go-lang bash
+    # shellcheck disable=SC1091
+    source /etc/profile.d/path.sh
 fi
 
 go mod tidy -go="$(curl -sL https://golang.org/VERSION?m=text | sed -n 's/go//;s/\..$//;1p')"
@@ -25,8 +25,8 @@ go mod tidy -go="$(curl -sL https://golang.org/VERSION?m=text | sed -n 's/go//;s
 # Update GitHub Action commit hashes
 gh_actions=$(grep -r "uses: [a-zA-Z\-]*/[\_a-z\-]*@" .github/ | sed 's/@.*//' | awk -F ': ' '{ print $3 }' | sort -u)
 for action in $gh_actions; do
-	commit_hash=$(git ls-remote "https://github.com/$action" | grep 'refs/tags/[v]\?[0-9][0-9\.]*$' | sed 's|refs/tags/[vV]\?[\.]\?||g' | sort -u -k2 -V | tail -1 | awk '{ printf "%s # %s\n",$1,$2 }')
-	# shellcheck disable=SC2267
-	grep -ElRZ "uses: $action@" .github/ | xargs -0 -l sed -i -e "s|uses: $action@.*|uses: $action@$commit_hash|g"
+    commit_hash=$(git ls-remote "https://github.com/$action" | grep 'refs/tags/[v]\?[0-9][0-9\.]*$' | sed 's|refs/tags/[vV]\?[\.]\?||g' | sort -u -k2 -V | tail -1 | awk '{ printf "%s # %s\n",$1,$2 }')
+    # shellcheck disable=SC2267
+    grep -ElRZ "uses: $action@" .github/ | xargs -0 -l sed -i -e "s|uses: $action@.*|uses: $action@$commit_hash|g"
 done
 make fmt
